c201c58fb520de6a622ac4328c3a4d93773a92e1
Febin Mubarak <febinmubarak333@gmail.com>
2022-05-05 07:39:08 +0400

    Unwanted

--- src/Components/RawPost/RawPost.js
+++ src/Components/RawPost/RawPost.js
@@ -1,43 +1,59 @@
 import React,{useEffect,useState} from 'react'
-import ReactPlayer from 'react-player/lazy'
+
 import "./RawPost.css"
 import axios from "../../axios"
 import {imageUrl,API_KEY} from "../../constants/Constants"
 function RawPost(props) {
     const [movies,setMovies] = useState([])
     const [urlId,seturlId] = useState("")
+    
     useEffect(() => {
       axios.get(props.url).then(response=>{
           console.log(response.data);
           setMovies(response.data.results) })
      }, [])
+     
     
      const HandleMovie = (id)=>{
          console.log(id)
          axios.get(`/movie/${id}/videos?api_key=${API_KEY}&language=en-US`).then(response=>{
-             
              if(response.data.results.length!==0){
-                    seturlId(response.data.results[0].key);
+                    seturlId(response.data.results[0]);
                     console.log(response.data)
              }else{
                  console.log("Empty array");
              }
          })
      }
-    
+     
      
      return (
         <div className='row'>
 
             <h2>{props.title}</h2>
             <div className="posters">
+            
+                
                 {movies.map((obj)=>
-                    <img onClick={()=>HandleMovie(obj.id)} className={props.isSmall?'smallPoster':'poster'} src={`${imageUrl+obj.backdrop_path}`} alt="poster" />
-                )}
+               
+                 <img onClick={()=>HandleMovie(obj.id)} className={props.isSmall?'smallPoster':'poster'} src={`${imageUrl+obj.poster_path}`} alt="poster" />
+                    
+               )}
+               
             </div>
+             
+        { urlId &&  <iframe className='video'
+           
+        title='Youtube player'
+        width={"100%"}
+        height={"450px"}
+        
+        
+        sandbox='allow-same-origin allow-forms allow-popups allow-scripts allow-presentation'cont
+        src={`https://youtube.com/embed/${urlId.key}?autoplay=${1}`}>
+</iframe>   }
             
-            { urlId && <ReactPlayer  url={urlId.key} />}
-            
+
         </div>
     )
 }
@@ -45,4 +61,4 @@ function RawPost(props) {
 
 
 
-export default RawPost
+export default RawPost 
